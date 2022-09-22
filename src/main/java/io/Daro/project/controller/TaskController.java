fb0d4972b4e68ff234acd9b4e57365fd81bf9f1a
daro <razorek89@gmail.com>
2022-09-22 16:56:25 +0200

    change RestController to Controller

--- src/main/java/io/Daro/project/controller/TaskController.java
+++ src/main/java/io/Daro/project/controller/TaskController.java
@@ -5,8 +5,10 @@ import io.Daro.project.model.Task;
 import io.Daro.project.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -14,29 +16,34 @@ import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+@Controller
+//@ResponseBody
 class TaskController {
 
     private final TaskRepository repository;
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
 
+    //@Autowired
     TaskController(final TaskRepository repository) {
         this.repository = repository;
     }
     //@RequestMapping(method = RequestMethod.GET)
-    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
+    @RequestMapping(value = "/tasks", params = {"!sort", "!page", "!size"}, method = RequestMethod.GET)
+    @ResponseBody
     ResponseEntity<List<Task>> readAllTasks(){
         logger.warn("Exposing all the task");
         return  ResponseEntity.ok(repository.findAll());
     }
 
-    @GetMapping("/tasks")
+    @RequestMapping(value = "/tasks", method = RequestMethod.GET)
+    @ResponseBody
     ResponseEntity<List<Task>> readAllTasks(Pageable page){
         logger.info("Custom pageable");
         return  ResponseEntity.ok(repository.findAll(page).getContent());
     }
 
-    @GetMapping("/tasks/{id}")
+    @RequestMapping(value = "/tasks/{id}", method = RequestMethod.GET)
+    @ResponseBody
     ResponseEntity<Task> readTask(@PathVariable int id){
         Optional<Task> res = repository.findById(id);
         /*if(!repository.existsById(id)){
@@ -45,14 +52,16 @@ class TaskController {
         return res.map(task -> ResponseEntity.ok(task)).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @PostMapping("/tasks")
+    @RequestMapping(value = "/tasks", method = RequestMethod.POST)
+    @ResponseBody
     ResponseEntity<Task> createTask(@RequestBody @Valid Task task){
         //return repository.save(task);
         Task created = repository.save(task);
         return ResponseEntity.created(URI.create(String.valueOf("/" + created.getId()))).body(created);
     }
 
-    @PutMapping("/tasks/{id}")
+    @RequestMapping(value = "/tasks/{id}", method = RequestMethod.PUT)
+    @ResponseBody
     ResponseEntity<?> updateTask(@PathVariable int id,  @RequestBody @Valid Task task) {
         if (!repository.existsById(id)){
             return ResponseEntity.notFound().build();
