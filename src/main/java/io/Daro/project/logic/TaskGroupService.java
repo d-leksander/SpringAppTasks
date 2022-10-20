10b03983ac308eeb86e0d58ba06baff5e03c7f9f
daro <razorek89@gmail.com>
2022-10-20 21:49:28 +0200

    Added checking open or close alle the tasks in group

--- src/main/java/io/Daro/project/logic/TaskGroupService.java
+++ src/main/java/io/Daro/project/logic/TaskGroupService.java
@@ -1,11 +1,11 @@
 package io.Daro.project.logic;
 
-import io.Daro.project.model.Task;
+import io.Daro.project.model.TaskConfigurationProperties;
 import io.Daro.project.model.TaskGroup;
 import io.Daro.project.model.TaskGroupRepository;
+import io.Daro.project.model.TaskRepository;
 import io.Daro.project.model.projection.GroupReadModel;
 import io.Daro.project.model.projection.GroupWriteModel;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,9 +15,13 @@ import java.util.stream.Collectors;
 public class TaskGroupService {
 
     private TaskGroupRepository repository;
+    //private TaskConfigurationProperties configurationProperties;
+    private TaskRepository taskRepository;
 
-    TaskGroupService(final TaskGroupRepository repository) {
+    TaskGroupService(final TaskGroupRepository repository, final TaskRepository taskRepository) {
         this.repository = repository;
+        //this.configurationProperties = configurationProperties;
+        this.taskRepository = taskRepository;
     }
     public GroupReadModel createGroup(GroupWriteModel source) {
         TaskGroup result = repository.save(source.toGroup());
@@ -28,5 +32,14 @@ public class TaskGroupService {
         return repository.findAll().stream()
                 .map(GroupReadModel::new)
                 .collect(Collectors.toList());
+    }
+
+    public void toggleGroup(int groupId){
+        if(taskRepository.existsByDoneIsFalseAndGroup_Id((groupId))){
+            throw new IllegalStateException("Group has not finished tasks. Must be closed all the tasks");
+        }
+        TaskGroup result = repository.findById(groupId)
+                .orElseThrow(()-> new IllegalArgumentException("Task group with given is not found"));
+        result.setDone((!result.isDone()));
     }
 }
