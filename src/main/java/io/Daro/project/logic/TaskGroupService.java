a556ba81bff538b818e2f04a66e92a8b157d79b8
daro <razorek89@gmail.com>
2022-10-30 23:29:22 +0100

    Add save groups during create group. This is necessary to tests, additional change

--- src/main/java/io/Daro/project/logic/TaskGroupService.java
+++ src/main/java/io/Daro/project/logic/TaskGroupService.java
@@ -41,5 +41,6 @@ public class TaskGroupService {
         TaskGroup result = repository.findById(groupId)
                 .orElseThrow(()-> new IllegalArgumentException("Task group with given is not found"));
         result.setDone((!result.isDone()));
+        repository.save(result);
     }
 }
