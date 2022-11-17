47f2571ad97c271d80a38121c75483d5a2098e63
daro <razorek89@gmail.com>
2022-11-17 22:47:52 +0100

    More consistent notation->simpler notation with RequestMapping

--- src/main/java/io/Daro/project/controller/InfoController.java
+++ src/main/java/io/Daro/project/controller/InfoController.java
@@ -3,9 +3,11 @@ package io.Daro.project.controller;
 import io.Daro.project.model.TaskConfigurationProperties;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/info")
 class InfoController {
     //@Value("${spring.datasource.url}")
     //private String url;
@@ -21,11 +23,11 @@ class InfoController {
 
     }
 
-    @GetMapping("/info/url")
+    @GetMapping("/url")
     String url(){
         return dataSourceProperties.getUrl();
     }
-    @GetMapping("/info/prop")
+    @GetMapping("/prop")
     boolean myProp(){
         return myProp.getTemplate().isAllowMultipleTasks();
     }
--- src/main/java/io/Daro/project/controller/TaskController.java
+++ src/main/java/io/Daro/project/controller/TaskController.java
@@ -21,6 +21,7 @@ import java.util.Optional;
 //@Controller
 //@ResponseBody
 @RestController
+@RequestMapping("/tasks")
 class TaskController {
 
     private final TaskRepository repository;
@@ -31,21 +32,21 @@ class TaskController {
         this.repository = repository;
     }
     //@RequestMapping(method = RequestMethod.GET)
-    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
+    @GetMapping(params = {"!sort", "!page", "!size"})
     //@ResponseBody
     ResponseEntity<List<Task>> readAllTasks(){
         logger.warn("Exposing all the task");
         return  ResponseEntity.ok(repository.findAll());
     }
 
-    @GetMapping(value = "/tasks")
+    @GetMapping
     //@ResponseBody
     ResponseEntity<List<Task>> readAllTasks(Pageable page){
         logger.info("Custom pageable");
         return  ResponseEntity.ok(repository.findAll(page).getContent());
     }
 
-    @GetMapping(value = "/tasks/{id}")
+    @GetMapping("/{id}")
     //@ResponseBody
     ResponseEntity<Task> readTask(@PathVariable int id){
         Optional<Task> res = repository.findById(id);
@@ -55,14 +56,19 @@ class TaskController {
         return res.map(task -> ResponseEntity.ok(task)).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @PostMapping(value = "/tasks")
+    @GetMapping("/search/done")
+    ResponseEntity<List<Task>> readDoneTasks(@RequestParam(defaultValue = "true") boolean state) {
+        return ResponseEntity.ok(repository.findByDone(state));
+    }
+
+    @PostMapping
     //@ResponseBody
     ResponseEntity<Task> createTask(@RequestBody @Valid Task task){
         //return repository.save(task);
         Task created = repository.save(task);
         return ResponseEntity.created(URI.create(String.valueOf("/" + created.getId()))).body(created);
     }
-    @PutMapping(value = "/tasks/{id}")
+    @PutMapping(value = "/{id}")
     //@ResponseBody
     ResponseEntity<?> updateTask(@PathVariable int id,  @RequestBody @Valid Task toUpdate) {
         if (!repository.existsById(id)){
@@ -77,7 +83,7 @@ class TaskController {
     }
 
     @Transactional
-    @PatchMapping(value = "/tasks/{id}")
+    @PatchMapping(value = "/{id}")
     //@ResponseBody
     public ResponseEntity<?> toggleTask(@PathVariable int id) {
         if (!repository.existsById(id)){
