9757601951eaf4858211f79f5e096f9d15c1f6db
daro <razorek89@gmail.com>
2022-11-23 18:05:22 +0100

    Add ModelAttribute

--- src/main/java/io/Daro/project/controller/ProjectController.java
+++ src/main/java/io/Daro/project/controller/ProjectController.java
@@ -1,19 +1,28 @@
 package io.Daro.project.controller;
 
+import io.Daro.project.model.ProjectSteps;
 import io.Daro.project.model.projection.ProjectWriteModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/projects")
 class ProjectController {
     @GetMapping
-    String showProject(Model model){
+    String showProject(Model model) {
         //var projectToEdit = new ProjectWriteModel();
         //projectToEdit.setDescription("test");
         model.addAttribute("project", new ProjectWriteModel());
+        return "projects";
+    }
+
+    @PostMapping(params = "addStep")
+    String addProjectStep(@ModelAttribute("project") ProjectWriteModel current) {
+        current.getSteps().add(new ProjectSteps());
         return "projects";
     }
 
