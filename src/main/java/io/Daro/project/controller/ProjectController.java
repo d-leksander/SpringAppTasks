9ceffc56874370022573bd8673e2d99e494bfe10
daro <razorek89@gmail.com>
2022-11-30 16:10:16 +0100

    Checking projects html and little correct

--- src/main/java/io/Daro/project/controller/ProjectController.java
+++ src/main/java/io/Daro/project/controller/ProjectController.java
@@ -4,15 +4,14 @@ import io.Daro.project.logic.ProjectService;
 import io.Daro.project.model.Project;
 import io.Daro.project.model.ProjectSteps;
 import io.Daro.project.model.projection.ProjectWriteModel;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
@@ -52,6 +51,23 @@ class ProjectController {
     @PostMapping(params = "addStep")
     String addProjectStep(@ModelAttribute("project") ProjectWriteModel current) {
         current.getSteps().add(new ProjectSteps());
+        return "projects";
+    }
+
+    @PostMapping("/{id}")
+    String createGroup(
+            @ModelAttribute("project") ProjectWriteModel current,
+            Model model,
+            @PathVariable int id,
+            @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm") LocalDateTime deadline
+    ) {
+        try {
+            service.createGroup(deadline, id);
+            model.addAttribute("message", "Dodano grupę!");
+            //model.addAttribute("message", "Dodano grupę!");
+        } catch (IllegalStateException | IllegalArgumentException e) {
+            model.addAttribute("message", "Błąd podczas tworzenia grupy!");
+        }
         return "projects";
     }
     @ModelAttribute("projects")//
