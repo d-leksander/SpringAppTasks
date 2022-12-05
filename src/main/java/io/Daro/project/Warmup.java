6e79b4f6e77e0c47557234492eaf23d601a0c77a
daro <razorek89@gmail.com>
2022-12-06 00:39:44 +0100

    Create class Warmup and added ApplicationListener

---
+++ src/main/java/io/Daro/project/Warmup.java
@@ -0,0 +1,45 @@
+package io.Daro.project;
+
+
+import io.Daro.project.model.Task;
+import io.Daro.project.model.TaskGroup;
+import io.Daro.project.model.TaskGroupRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+class Warmup implements ApplicationListener<ContextRefreshedEvent> {
+    public static final Logger logger = LoggerFactory.getLogger(Warmup.class);
+
+    private final TaskGroupRepository groupRepository;
+
+    Warmup(final TaskGroupRepository groupRepository) {
+        this.groupRepository = groupRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(final ContextRefreshedEvent event) {
+
+        logger.info("Application warmup after context refreshed");
+        final String description = "ApplicationContextEvent";
+        if(!groupRepository.existsByDescription(description)) {
+            logger.info("No required group found. Add group");
+            var group = new TaskGroup();
+            group.setDescription(description);
+            group.setTasks(Set.of(
+                    new Task("ContextClosedEvent", null, group),
+                    new Task("ContextRefreshedEvent", null, group),
+                    new Task("ContextStoppedEvent", null, group),
+                    new Task("ContextStartedEvent", null, group)
+            ));
+            groupRepository.save(group);
+        }
+
+
+    }
+}
