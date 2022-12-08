e4aad45e31cb0eb7eb2d8b60934e0995acc3ff87
daro <razorek89@gmail.com>
2022-12-09 00:56:58 +0100

    Add Async annotation->running process on multi threads

--- src/main/java/io/Daro/project/reports/ChangedTaskEventListener.java
+++ src/main/java/io/Daro/project/reports/ChangedTaskEventListener.java
@@ -7,6 +7,7 @@ import io.Daro.project.model.event.TaskUndone;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,11 +20,13 @@ class ChangedTaskEventListener {
         this.repository = repository;
     }
 
+    @Async
     @EventListener
     public void on(TaskDone event){
         onChanged(event);
     }
 
+    @Async
     @EventListener
     public void on(TaskUndone event){
         //logger.info("Got " + event);
