37d76cd333065f510484000216673bb355fc0d73
daro <razorek89@gmail.com>
2022-12-20 16:44:07 +0100

    Add authenticationProvider Keycloak

--- src/main/java/io/Daro/project/security/SecurityConfiguration.java
+++ src/main/java/io/Daro/project/security/SecurityConfiguration.java
@@ -2,8 +2,11 @@ package io.Daro.project.security;
 
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
@@ -13,8 +16,18 @@ class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
 
     @Bean
     KeycloakSpringBootConfigResolver keycloakConfigResolver(){
-            return new KeycloakSpringBootConfigResolver();
-        }
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+    void configureGlobal(AuthenticationManagerBuilder auth){
+        var authorityMapper = new SimpleAuthorityMapper();
+        authorityMapper.setPrefix("ROLE_");
+        authorityMapper.setConvertToUpperCase(true);
+        KeycloakAuthenticationProvider keycloakProvider = keycloakAuthenticationProvider();
+        keycloakProvider.setGrantedAuthoritiesMapper(authorityMapper);
+        auth.authenticationProvider(keycloakProvider);
+    }
+
 
     @Override
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
